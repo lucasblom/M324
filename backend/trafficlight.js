e0c571b1a96abb88354f9c1b50d51ad2773a1e0a
Lucas Blom <51747171+SkateFastEatTrash@users.noreply.github.com>
2023-12-06 14:57:38 +0100

    Docker Traffic Light App

--- backend/trafficlight.js
+++ backend/trafficlight.js
@@ -24,19 +24,19 @@ WALK
 'use strict';
 
 function trafficLight(color){
-    if (color === undefined){
-        return `OUT OF ORDER`
-    } else {
-        if (color === 'red'){
-            return `DON'T WALK!`
-        }
-        else if (color === 'orange'){
-            return `ATTENTION!`
-        }
-        else if (color === 'green'){
-            return `WALK!`
-        }
-    }
+    if (color === undefined){//??
+        return `OUT OF ORDER`//??
+    } else {//??
+        if (color === 'red'){//??
+            return `DON'T WALK!`//??
+        }//??
+        else if (color === 'orange'){//??
+            return `ATTENTION!`//??
+        }//??
+        else if (color === 'green'){//??
+            return `WALK!`//??
+        }//??
+    }//??
 }
 
 module.exports = {
